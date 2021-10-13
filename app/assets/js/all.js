f0bf762717a265f76efc64c1cb65607018754c36
Tansy <cwhu823@icloud.com>
2021-10-13 19:28:18 +1300

    add animation/AOS/jQUery

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,12 @@ console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+
+   lightbox.option({
+      resizeDuration: 700,
+      wrapAround: true,
+      fadeDuration: 750,
+      positionFromTop: 200,
+    })
 });
+
