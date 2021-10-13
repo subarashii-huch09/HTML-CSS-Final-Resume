3123662e0cf17c349150c17ba75483734c50ed12
Tansy <cwhu823@icloud.com>
2021-10-13 19:27:32 +1300

    Update 2021-10-13T06:27:30.795Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,5 +3,11 @@
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  lightbox.option({
+    resizeDuration: 700,
+    wrapAround: true,
+    fadeDuration: 750,
+    positionFromTop: 200
+  });
 });
 //# sourceMappingURL=all.js.map
